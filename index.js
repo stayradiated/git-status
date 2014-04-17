9f151b9b8bfc2b7f2b00187c255665fa5b9db76a
George Czabania <george@czabania.com>
2014-04-17 20:09:26 +1200

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var spawn = require('child_process').spawn;
+var path = require('path');
+var fs = require('fs');
+
+module.exports = function (options, fn) {
+  options = options || {};
+
+  var output = {};
+  var cwd = options.cwd || process.cwd();
+  var args = ['status', '-s'];
+  if (options.branch) args.push('-b');
+
+  var checkStatus = function (dir) {
+    var process = spawn('git', args, {
+      cwd: path.join(cwd, dir)
+    });
+
+    process.stdout.on('data', function (data) {
+      fn(dir, data.toString());
+    });
+  };
+
+  fs.readdir(cwd, function (err, contents) {
+    contents.forEach(checkStatus);
+  });
+
+};
